e2c24272856af0fd9c0906fb047cd3eea4bf88fd
unknown <john_riley2@student.uml.edu>
2021-09-15 13:50:21 -0400

    Corrected bug

--- src/App.js
+++ src/App.js
@@ -1,57 +1,45 @@
-import React from 'react';
+import React from "react";
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Redirect,
-} from 'react-router-dom';
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 // import { MuiThemeProvider } from '@material-ui/core';
 
-import NFY from './views/NFY';
-import BitAlliance from './views/BitAlliance';
-import Tek from './views/tek';
+import NFY from "./views/NFY";
+import BitAlliance from "./views/BitAlliance";
+import Tek from "./views/tek";
 // import ChurcheOfTheChain from './views/ChurcheOfTheChain';
 // import Eros from './views/Eros';
 // import Mimir from './views/Mimir';
 // import Pantheon from './views/Pantheon';
-import Layout from './layout';
-import Diamond from './views/Diamond';
+import Layout from "./layout";
+import Diamond from "./views/Diamond";
 
-import './App.css';
+import "./App.css";
 
 function App() {
-    return (
-        <Router>
-            <Switch>
-                <Route path="/" exact>
-                    <Layout>
-                        <NFY />
-                    </Layout>
-                </Route>
-                <Route path="/nfy" exact>
-                    <Layout>
-                        <NFY />
-                    </Layout>
-                </Route>
-                <Route path="/diamond" exact>
-                    <Layout>
-                        <Diamond />
-                    </Layout>
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Layout>
+            <NFY />
+          </Layout>
+        </Route>
+        <Route path="/nfy" exact>
+          <Layout>
+            <NFY />
+          </Layout>
+        </Route>
+        <Route path="/diamond" exact>
+          <Layout>
+            <Diamond />
+          </Layout>
 
-                    <Route path="/tek" exact>
-                    <Layout>
-                        <Tek />
-                    </Layout>
-                </Route>
-                <Route path="/bit-alliance" exact>
-                    <Layout>
-                        <BitAlliance />
-                    </Layout>
-                </Route>
-
-                
-                {/* </Route>
+          {/* </Route>
                 <Route path="/churche-of-the-chain" exact>
                     <Layout>
                         <ChurcheOfTheChain />
@@ -71,15 +59,25 @@ function App() {
                     <Layout>
                         <Pantheon />
                     </Layout> */}
-                </Route> */}
-                <Redirect to="/nfy">
-                    <Layout>
-                        <NFY />
-                    </Layout>
-                </Redirect>
-            </Switch>
-        </Router>
-    );
+        </Route>
+        <Route path="/tek" exact>
+          <Layout>
+            <Tek />
+          </Layout>
+        </Route>
+        <Route path="/bit-alliance" exact>
+          <Layout>
+            <BitAlliance />
+          </Layout>
+        </Route>
+        <Redirect to="/nfy">
+          <Layout>
+            <NFY />
+          </Layout>
+        </Redirect>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
